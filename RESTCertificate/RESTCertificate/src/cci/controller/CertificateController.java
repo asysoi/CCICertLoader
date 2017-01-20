5582584933be7ba45a62d7eecdfc9f91ecdc997a
asysoi <asysoi@cci.by>
2017-01-20 20:38:26 +0200

    Update

--- RESTCertificate/RESTCertificate/src/cci/controller/CertificateController.java
+++ RESTCertificate/RESTCertificate/src/cci/controller/CertificateController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,6 +37,7 @@ import cci.model.OwnCertificateHeaders;
 import cci.model.OwnCertificates;
 import cci.service.OwnCertificateService;
 
+@Controller
 @RestController
 public class CertificateController {
 
